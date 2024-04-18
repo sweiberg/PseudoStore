27779fe1a1fcdd538bafd9de90183b6c356c758e
Stephen Weiberg <sweiberg@ufl.edu>
2024-04-18 01:21:44 -0400

    Updated trend buttons

--- web/src/pages/dashboard/trend/trend3.js
+++ web/src/pages/dashboard/trend/trend3.js
@@ -1,67 +1,94 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Line } from "react-chartjs-2";
+import Button from "../buttons/button"
 
-const name = "Promo Effectiveness";
+function Trend3() {
+  const name = "Promotion Effectiveness";
 
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June"],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "#FFF",
-      borderColor: '#FFF',
-      color:  '#FFF',
-      data: [0, 20, 5, 22, 30, 40, 55],
+
+  // Either pre-fill the button lists or API sends an array of them
+  const dropdown = {
+    first: {
+      title: "Promotion",
+      list: [{ id: 1, name: 'Promo 1' }, { id: 2, name: 'Promo 2' }],
+      selected: { id: 1, name: 'Promo 1' }
     },
-  ],
-};
-
-const options = {
-  plugins: {
-      legend: {
-        position: "bottom",
-        align: "end",
-        labels: {
-          color: "#FFF",
+    second: {
+      title: "Type",
+      list: [{ id: 1, name: 'Item Quantity' }, { id: 2, name: 'Product Sales' }],
+      selected: { id: 1, name: 'Item Quantity' }
+    }
+  };
+  
+  const handleSelectionChange = (identifier, item) => {
+    setSelections(prevSelections => ({
+      ...prevSelections,
+      [identifier]: item
+    }));
+  };
+  
+  const [selections, setSelections] = useState({
+    first: dropdown.first.selected,
+    second: dropdown.second.selected
+  });
+  
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: `${dropdown.first.title}: ${selections.first.name}, ${dropdown.second.title}: ${selections.second.name}`,
+        backgroundColor: "#FFF",
+        borderColor: '#FFF',
+        color:  '#FFF',
+        data: [0, 20, 5, 22, 30, 40, 55],
+      },
+    ],
+  };
+  
+  const options = {
+    plugins: {
+        legend: {
+          position: "bottom",
+          align: "end",
+          labels: {
+            color: "#FFF",
+          }
+        }
+      },
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        border: {
+          dash: [4, 4],
+          display: false,
+        },
+        grid: {
+          color: 'rgba(255,255,255,0.2)',
+          tickBorderDash: [4, 4],
+          drawTicks: true,
+          drawOnChartArea: true
+        },
+        ticks: {
+            color: "rgba(255,255,255,0.75)",
+            stepSize: 5
+        },
+      },
+      x: {
+        border: {
+          display: false,
+        },
+        grid: {
+          display: false
+        },
+        ticks: {
+            color: 'rgba(255,255,255,0.75)',
+            stepSize: 5
         }
       }
-    },
-  maintainAspectRatio: false,
-  scales: {
-    y: {
-      border: {
-        dash: [4, 4],
-        display: false,
-      },
-      grid: {
-        color: 'rgba(255,255,255,0.2)',
-        tickBorderDash: [4, 4],
-        drawTicks: true,
-        drawOnChartArea: true
-      },
-      ticks: {
-          color: "rgba(255,255,255,0.75)",
-          stepSize: 5
-      },
-    },
-    x: {
-      border: {
-        display: false,
-      },
-      grid: {
-        display: false
-      },
-      ticks: {
-          color: 'rgba(255,255,255,0.75)',
-          stepSize: 5
-      }
     }
-  }
-};
+  };
 
-const Trend3 = () => {
   return (
-    <div className="px-4 md:px-10 mx-auto w-full -m-24">
       <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
@@ -85,13 +112,18 @@ const Trend3 = () => {
                   <div className="p-4 flex-auto"><div className="relative h-650-px">Test</div></div>
               </div>
           </div>
-      </div>
-      <div className="flex flex-wrap mt-4">
+          <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 xl:mb-0 px-4">
-          Test
+          {Object.entries(dropdown).map(([key, { title, list, selected }]) => (
+            <Button key={key}
+              title={title}
+              list={list}
+              onSelectionChange={(item) => handleSelectionChange(key, item)}
+            />
+          ))}
         </div>
       </div>
-    </div>
+      </div>
   );
 };
 
