b77eeb03cb42be8d6997eabe90e48c8bfc588702
jweiberg <jonweiberg@gmail.com>
2024-04-24 07:45:52 -0400

    Update five.go

--- server/trend/five.go
+++ server/trend/five.go
@@ -13,30 +13,29 @@ func GetTrendFive(season string, categoryName string, lowDate string, highDate s
 
 	err := db.Oracle.Raw(`
 		SELECT 
-			TO_CHAR(o.Created_At, 'YYYY-MM') 
-			AS Order_Month, pm.Method, COUNT(DISTINCT o.ID) 
-			AS Multi_Count
+			TO_CHAR(o.created_at, 'YYYY-MM') AS Order_Month,
+			pm.Method, COUNT(DISTINCT o.ID) AS Multi_Count
 		FROM 
-			Categories c
+			orders o
 		JOIN 
-			Products p ON c.ID = p.Category_ID
-		JOIN 
-			Orders o ON p.ID = o.Product_ID
+			Products p ON o.Product_ID = p.ID 
 		JOIN 
 			Payments pm ON o.ID = pm.Order_ID
+		JOIN 
+			Categories c ON p.Category_ID = c.ID
 		WHERE 
-			o.Created_At BETWEEN TO_DATE(?, 'YYYY-MM') AND TO_DATE(?, 'YYYY-MM')
+			Line_Number > ?
 		AND 
 			p.Season = ?
 		AND 
 			c.Name = ?
+		AND 
+			o.created_at BETWEEN TO_DATE(?, 'YYYY-MM') AND TO_DATE(?, 'YYYY-MM')
 		GROUP BY 
-			c.Name, TO_CHAR(o.Created_At, 'YYYY-MM'), pm.Method
-		HAVING 
-			COUNT(DISTINCT o.Product_ID) > ?
+			TO_CHAR(o.created_at, 'YYYY-MM'), pm.Method
 		ORDER BY 
 			Order_Month, Multi_Count DESC
-	`, lowDate, highDate, season, categoryName, itemCount).Scan(&results).Error
+	`, itemCount, season, categoryName, lowDate, highDate).Scan(&results).Error
 
 	if err != nil {
 		return nil, err
